653fc4004fe6104cc2005156276e34b7c9abba0e
chochob000 <chochob000@gmail.com>
2022-03-22 13:19:16 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    // contoh comment
+    $nama = "Andi";
+    $umur = 25;
+    
+    // tuliskan "Andi berumur 25 Tahun"
+    echo $nama." berumur ".$umur." Tahun<br />";
+    echo "$nama berumur $umur Tahun<br />";
+    echo '$nama berumur $tahun Tahun';
+
+    $umur = "25 Tahun";
+    echo $umur;
